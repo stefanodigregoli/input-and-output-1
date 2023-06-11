19bd01b62f8282e896a54dddb258bf882145882b
ariosto22 <emanuele.digregoli40@gmail.com>
2023-06-11 15:54:24 +0200

    changed string name variable

--- src/PrintYourName.java
+++ src/PrintYourName.java
@@ -1,8 +1,6 @@
 public class PrintYourName {
     public static void main(String[] args) {
-        String name  ="Stefano";
-        String surname = "Di Gregoli";
-
-        System.out.println(name + " " + surname);
+        String yourName  ="Stefano";
+        System.out.println(yourName);
     }
 }
