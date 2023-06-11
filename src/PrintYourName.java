a6422aba2fa4763b93a28db96663f9abb27950d2
ariosto22 <emanuele.digregoli40@gmail.com>
2023-06-11 15:48:39 +0200

    Input and output - 1 create a class called PrintYourName provide the class PrintYourName with a main method: public static void main(String[] args) { ... } inside the main method write the code that prints your name. Note: in the console, after your name, you will notice a message like the following:
    Process finished with exit code 0
    Don't worry, it means that you didn't break Java, so there were 0 errors :)

---
+++ src/PrintYourName.java
@@ -0,0 +1,8 @@
+public class PrintYourName {
+    public static void main(String[] args) {
+        String name  ="Stefano";
+        String surname = "Di Gregoli";
+
+        System.out.println(name + " " + surname);
+    }
+}
